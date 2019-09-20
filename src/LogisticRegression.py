c09f52910b111a396d7a73cf294398c90b78ce65
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-09-19 22:26:33 -0400

    functional, better arpgarse and 2 figures

--- src/LogisticRegression.py
+++ src/LogisticRegression.py
@@ -33,12 +33,12 @@ class LogisticRegression():
 		"""Step in iteration, returns curr theta and j"""
 		H_theta=np.divide(1,1+np.exp(-self.X.dot(self.theta)))
 		e=H_theta-self.Y
-		j=1/m * np.sum(-self.Y*np.log(H_theta) - (1-Y)*np.log(1-H_theta))
+		j=1/self.m * np.sum(-self.Y*np.log(H_theta) - (1-self.Y)*np.log(1-H_theta))
 		self.theta-=self.alpha*(self.X.T.dot(e))
 		return self.theta.reshape(self.n+1),j
 	def accuracy(self):
 		H_theta=np.divide(1,1+np.exp(-self.X.dot(self.theta)))
-		prediction= H_theta>=5
+		prediction= H_theta>=0.5
 		currect=prediction==self.Y
 		return np.sum(currect)/self.m
 def normalize_vec(X):
--- src/main.py
+++ src/main.py
@@ -2,32 +2,57 @@ import numpy as np
 import matplotlib.pyplot as  plt
 import argparse
 from LogisticRegression import LogisticRegression
+from ast import literal_eval
+from collections import namedtuple
 
+ModelEntry=namedtuple("ModelEntry",["model","errs","accrs"])
+
+def interval(s):
+	global GRAPH_TITLE
+	ans=literal_eval(s)
+	if isinstance(ans,tuple) and len(ans)==3 and all(isinstance(x, (float,int)) for x in ans):
+		a,b,c=ans
+		GRAPH_TITLE=rf'Gráfico de $J(\theta)$ para diferentes alphas que variam ${a}$ e ${b}$, passo {c}'
+		return np.arange(a,b+c,c)
+	if isinstance(ans,list) and all(isinstance(x, (float,int)) for x in ans):
+		GRAPH_TITLE=rf'Gráfico de $J(\theta)$ para os alphas {ans}'
+		return ans
+	raise TypeError()
+GRAPH_TITLE=""
 parser=argparse.ArgumentParser()
 parser.add_argument("--no_it","-n",type=int,required=True)
-parser.add_argument("--interval","-i",type=str,required=True)
+parser.add_argument("--interval","-i",type=interval,required=True)
+parser.add_argument("--normalize",action="store_true")
 args=parser.parse_args()
 
-a,b,c=[float(o) for o in args.interval.split(",")]
-
-entradas=np.genfromtxt('../samples/entradas_x.csv', delimiter=',').T
-saidas=np.genfromtxt('../samples/saidas_y.csv', delimiter=',')
+entradas=np.genfromtxt('../samples/entradas_x.txt', delimiter=',').T
+saidas=np.genfromtxt('../samples/saidas_y.txt', delimiter=',')
 
 plt.rc('text', usetex=True)
-xs=np.array(range(1,args.no_it+1))
-ans=[]
-for alpha in np.arange(a,b+c,c):
+entries=[]
+for alpha in args.interval:
 	model=LogisticRegression(*entradas,saidas,alpha=alpha,normalize=True)
 	errs=np.zeros((args.no_it))
+	acrs=errs.copy()
 	for i in range(args.no_it):
+		acrs[i]=model.accuracy()
 		thetao,j=next(model)
 		errs[i]=j
-	ans.append([alpha,j,*thetao])
-	plt.plot(xs,errs,label=rf"$\alpha={alpha}$")
-header=";".join(["alpha","j",*(f"t{i}" for i in range(12))])
-np.savetxt("results.csv", ans, delimiter=";",header=header)
+	entries.append(ModelEntry(model,errs,acrs))
+
+xs=np.array(range(1,args.no_it+1))
+for entry in entries:
+	plt.plot(xs,entry.errs,label=rf"$\alpha={entry.model.alpha}$")
 plt.legend(fontsize=8)
 plt.xlabel('Números de iteração')
 plt.ylabel(r'$J(\theta)$')
-plt.title(rf'Gráfico de $J(\theta)$ para diferentes alphas que variam ${a}$ e ${b}$, passo {c}')
+plt.title(GRAPH_TITLE)
+
+plt.figure()
+for entry in entries:
+	plt.plot(xs,entry.accrs,label=rf"$\alpha={entry.model.alpha}$")
+plt.legend(fontsize=8)
+plt.xlabel('Números de iteração')
+plt.ylabel(r'acurácia')
+plt.title(GRAPH_TITLE.replace(r"$J(\theta)$","acurácia"))
 plt.show()
